b97d5262c31d63fcf6fe0a81672c7e2fe4439f6e
wnifirami <rami.ounifi@esprit.tn>
2021-07-28 19:33:37 +0100

    initiate the app and ressources

---
+++ DevelopersApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DevelopersApp
+//
+//  Created by Rami Ounifi on 28/7/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DevelopersApp/DevelopersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersAppApp.swift
+//  DevelopersApp
+//
+//  Created by Rami Ounifi on 28/7/2021.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
